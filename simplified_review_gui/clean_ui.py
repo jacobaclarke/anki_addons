c534807362b65408a2cd0e17a161ddd6168f29e7
Jacob Clarke <jacobclarke718@gmail.com>
2019-04-10 09:36:57 -0500

    added black toolbar and combined with some features of big colorful buttons

--- simplified_review_gui/clean_ui.py
+++ simplified_review_gui/clean_ui.py
@@ -11,13 +11,22 @@ from aqt.deckbrowser import DeckBrowser
 from anki.hooks import wrap
 from aqt.reviewer import Reviewer
 
+this_script_dir = path.dirname(__file__)
+user_css_path = path.join(this_script_dir, 'user_bottom_buttons.css')
+
+
+with open(user_css_path, encoding='utf-8') as f:
+    bottom_buttons_css = f.read()
 
 js_append_css_toolbar = "$('head').append(`<style>body {{ display: {};}}</style>`);"
+js_append_css = f"$('head').append(`<style>{bottom_buttons_css}</style>`);"
 
 def reviewer_ui(*args, **kwargs):
     mw.toolbar.web.eval(js_append_css_toolbar.format('none'))
     mw.menuBar().hide()
     mw.toolbar.web.setFixedHeight(0)
+    mw.reviewer.bottom.web.eval(js_append_css)
+    
 
 
 
